72084d4ce86f9233eed5a1a33fefcd37fafee61b
aekiratli <emre719@gmail.com>
2023-03-23 00:48:18 +0300

    removed unused code

--- middleware/auth.py
+++ middleware/auth.py
@@ -1,12 +1,6 @@
 from sanic import Request
 from sanic.exceptions import Unauthorized
-import jwt
-from config import SECRET_KEY
 from components.user.repository import extract_user_from_token
-
-# def check_token(request: Request) -> None:
-#     if not request.token:
-#         raise Unauthorized("JWT token is required.", status_code=401)
 
 UNPROTECTED_PATHS = [
     '/api/v1/users/login',
